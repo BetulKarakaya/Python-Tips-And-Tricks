9f00462531e0a2b40b5125cefed9355d45839c25
programmewithkarsan <106507209+programmewithkarsan@users.noreply.github.com>
2022-09-06 23:03:53 +0300

    Add files via upload

---
+++ enumerate_github.py
@@ -0,0 +1,25 @@
+
+#"Enumerate() method adds a counter to an iterable and returns it in a form of enumerating object." - https://www.geeksforgeeks.org/enumerate-in-python/
+
+
+fruits = ["apple","banana","cherry","strawberry","pineapple"]
+
+
+# you access array elements via loop and increase index manually.
+index = 0
+for item in fruits:
+	print(index,item)
+	index = index +1
+
+print()
+
+# you access array elements one by one via index of loop .
+for index in range(len(fruits)):
+	print(index, fruits[index])
+
+print()
+
+#CLEAR CODE VERSION
+#you may use enumerate() method that return both index and value. enumerate() method return tuple type value (index,value).  
+for index,item in enumerate(fruits):
+	print(index,item)
