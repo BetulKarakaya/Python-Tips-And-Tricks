b1c98963dca67ce575f600f58d56f4d037fc0b59
programmewithkarsan <106507209+programmewithkarsan@users.noreply.github.com>
2023-12-13 07:53:31 +0300

    Add files via upload

---
+++ slicing_list_github.py
@@ -0,0 +1,12 @@
+
+if __name__ == "__main__":
+
+    words = "Do You Want To See Something Great"
+    reverse_word = words[::-1]
+
+    print(reverse_word)
+
+    numbers = [1,2,3,4,5,6,7]
+    reverse_numbers = numbers[::-1]
+
+    print(reverse_numbers)
