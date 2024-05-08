e62812be8599286b106ec51fc3b6078b6434ef94
Betül Karakaya <106507209+BetulKarakaya@users.noreply.github.com>
2024-05-08 14:23:22 +0300

    Add files via upload

---
+++ bin_func_github.py
@@ -0,0 +1,10 @@
+# This Python script demonstrates the usage of the bin() function.
+# The bin() function converts an integer number to a binary string prefixed with "0b".
+# It is useful for converting integer numbers into binary representation.
+
+# Convert an integer to a binary string
+number = 42
+binary_representation = bin(number)
+
+# Print the binary representation
+print("Binary representation of", number, "is:", binary_representation)
