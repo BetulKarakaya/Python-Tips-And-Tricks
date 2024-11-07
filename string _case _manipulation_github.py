566e06eafbc0e855ef08bc174862b5104ba68ab1
Betül Karakaya <106507209+BetulKarakaya@users.noreply.github.com>
2024-11-07 12:32:08 +0300

    Add files via upload

---
+++ string _case _manipulation_github.py
@@ -0,0 +1,9 @@
+
+if __name__ == "__main__":
+
+    test_text = "hEllo worlD"
+
+    print(test_text.capitalize()) # - Capitalize only the first letter; all other characters will be lowercase
+    print(test_text.upper()) # - Convert all characters to uppercase
+    print(test_text.lower()) # - Convert all characters to lowercase
+    print(test_text.title()) # - Capitalize the first letter of each word; all other letters will be lowercase
