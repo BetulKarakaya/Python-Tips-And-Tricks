d6bd48fec251f592be33db317af64a817227062e
Betül Karakaya <106507209+BetulKarakaya@users.noreply.github.com>
2024-11-15 23:12:25 +0300

    Add files via upload

---
+++ dict_update_github.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    
+    #Adds or updates key-value pairs from another dictionary.
+    my_dict = {"name": "Betül"}
+    my_dict.update({"job": "Software Developer", "fav-language": "Python"})
+    print(my_dict)  # Output: {"name": "Betül", "job": "Software Developer", "fav-language": "Python"}
+ 
