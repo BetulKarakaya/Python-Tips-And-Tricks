d4e8d546d8dad2556f75a3a53a7793d0ac3a5997
Betül Karakaya <106507209+BetulKarakaya@users.noreply.github.com>
2025-08-05 23:59:20 +0300

    Add files via upload

--- enumerate_github.py
+++ enumerate_github.py
@@ -1,22 +1,4 @@
+fruits = ["apple", "banana", "cherry"]
 
-fruits = ["apple","banana","cherry","strawberry","pineapple"]
-
-
-# you access array elements via loop and increase index manually.
-index = 0
-for item in fruits:
-	print(index,item)
-	index = index +1
-
-print()
-
-# you access array elements one by one via index of loop .
-for index in range(len(fruits)):
-	print(index, fruits[index])
-
-print()
-
-#CLEAR CODE VERSION
-#you may use enumerate() method that return both index and value. enumerate() method return tuple type value (index,value).  
-for index,item in enumerate(fruits):
-	print(index,item)
+for i, fruit in enumerate(fruits):
+    print(f"{i}. fruit is {fruit}")
